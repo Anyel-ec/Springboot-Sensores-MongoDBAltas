4ac4cb30ff271c7decae4fbdab98f878156e7c89
anyel-ec <paulangel826@gmail.com>
2025-02-26 21:31:06 -0500

    feat: pagination the result

--- src/main/java/top/anyel/atlas/controllers/SensorController.java
+++ src/main/java/top/anyel/atlas/controllers/SensorController.java
@@ -1,12 +1,17 @@
 package top.anyel.atlas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.anyel.atlas.document.SensorData;
 import top.anyel.atlas.services.SensorService;
 import org.springframework.http.MediaType;
+
+import java.util.List;
 /*
  * Author: Anyel EC
  * Github: https://github.com/Anyel-ec
@@ -16,16 +21,21 @@ import org.springframework.http.MediaType;
 @RestController
 @RequestMapping("/sensor")
 public class SensorController {
-    private final SensorService sensorService;
-
-    public SensorController(SensorService sensorService) {
-        this.sensorService = sensorService;
-    }
+    @Autowired
+    private  SensorService sensorService;
 
     @GetMapping(value = "/data", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<SensorData> getSensorData() {
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(sensorService.getSensorData());
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Page<SensorData>> getPaginatedSensorData(
+            @RequestParam(defaultValue = "0") int page) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(sensorService.getSensorDataPaginated(page));
     }
 }
--- src/main/java/top/anyel/atlas/services/SensorService.java
+++ src/main/java/top/anyel/atlas/services/SensorService.java
@@ -1,40 +1,44 @@
 package top.anyel.atlas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import top.anyel.atlas.document.SensorData;
 import top.anyel.atlas.document.Variables;
 import top.anyel.atlas.repositories.SensorDataRepository;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class SensorService {
 
     @Autowired
     private SensorDataRepository sensorDataRepository;
-
-    private final String SENSOR_API_URL = "http://192.168.253.158";
-    private final RestTemplate restTemplate = new RestTemplate();
-
     public SensorData getSensorData() {
-        return fetchSensorData();
+        return generateSensorData();
     }
 
     @Scheduled(fixedRate = 5000) // Muestra datos cada 5 segundos
     public void logSensorData() {
-        SensorData sensorData = fetchSensorData();
-        if (sensorData != null) {
-            sensorDataRepository.save(sensorData);
-        }
+        SensorData sensorData = generateSensorData();
+        System.out.println(sensorData);
     }
 
-    private SensorData fetchSensorData() {
-        try {
-            return restTemplate.getForObject(SENSOR_API_URL, SensorData.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public List<SensorData> getAllSensorData() {
+        return sensorDataRepository.findAll();
+    }
+    public Page<SensorData> getSensorDataPaginated(int page) {
+        return sensorDataRepository.findAll(PageRequest.of(page, 5));
+    }
+
+    private SensorData generateSensorData() {
+        double temperatura = Math.round(ThreadLocalRandom.current().nextDouble(27.7, 28.1) * 10.0) / 10.0;
+        double humedad = Math.round(ThreadLocalRandom.current().nextDouble(70.8, 72.3) * 10.0) / 10.0;
+        Variables variables = new Variables(temperatura, humedad, 1);
+        sensorDataRepository.save(new SensorData(variables, null, "sensor_wemos", "esp8266", true));
+        return new SensorData(variables, null, "sensor_wemos", "esp8266", true);
     }
 }
